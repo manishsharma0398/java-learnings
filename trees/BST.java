8b3c2d4c2929c5466d6497643ed0413a0c14f5bd
Manish Sharma <manish.sharma0398@gmail.com>
2024-06-26 22:48:37 +0530

    BST

--- trees/BST.java
+++ trees/BST.java
@@ -36,8 +36,57 @@ public class BST {
         inOrderTraversal(root.right);
     }
 
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data > key) { // search left subtree
+            search(root.left, key);
+        } else if (root.data == key) {
+            return true;
+        } else {
+            search(root.right, key);
+        }
+
+        return false;
+    }
+
+    public static Node deleteNode(Node root, int val) {
+        if (root.data > val) {
+            root.left = deleteNode(root.left, val);
+        } else if (root.data < val) {
+            root.right = deleteNode(root.right, val);
+        } else {
+            // case1 - leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = deleteNode(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node inorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
@@ -45,5 +94,20 @@ public class BST {
         }
 
         inOrderTraversal(root);
+
+        // System.out.println("\nsearch 5 in BST: " + search(root, 5));
+
+        deleteNode(root, 4);
+        System.out.println();
+        inOrderTraversal(root);
+
+        deleteNode(root, 10);
+        System.out.println();
+        inOrderTraversal(root);
+
+        deleteNode(root, 5);
+        System.out.println();
+        inOrderTraversal(root);
+
     }
 }
