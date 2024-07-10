f264fa8019f7f775d674aa394f82b819e559e9a5
Manish Sharma <manish.sharma0398@gmail.com>
2024-07-10 08:30:53 +0530

    Radix Sort

--- sorting/RadixSort.java
+++ sorting/RadixSort.java
@@ -18,7 +18,6 @@ public class RadixSort {
     }
 
     static void countSort(int[] arr, int n, int pos) {
-        int[] sortedArr = new int[n];
         int[] countArr = new int[10];
         Arrays.fill(countArr, 0);
         for (int i = 0; i < n; i++) {
@@ -27,6 +26,9 @@ public class RadixSort {
         for (int i = 1; i < 10; i++) {
             countArr[i] += countArr[i - 1];
         }
+
+        int[] sortedArr = new int[n];
+
         for (int i = n - 1; i >= 0; i--) {
             sortedArr[--countArr[(arr[i] / pos) % 10]] = arr[i];
         }
@@ -36,7 +38,7 @@ public class RadixSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 432, 8, 530, 90, 88, 231, 11, 45, 677, 199 };
+        int[] arr = { 432, 8, 530, 90, 88, 231, 11, 45, 677, 199, 386, 549 };
         int maxNumber = getMax(arr);
         for (int pos = 1; (maxNumber / pos) > 0; pos *= 10) {
             countSort(arr, arr.length, pos);
